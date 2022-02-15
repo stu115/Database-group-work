8782e80abd129736eb3cb9e08a8c5e8c5c0399d1
Stefan Ahmed <32064735+stu115@users.noreply.github.com>
2022-02-15 17:16:54 +0000

    Add files via upload

---
+++ sql/drop tables - Alex, Ola, Baderin.sql
@@ -0,0 +1,26 @@
+
+DROP TABLE sponsored;
+
+DROP TABLE organization;
+
+DROP TABLE team_project;
+
+DROP TABLE attendance;
+
+DROP TABLE student;
+
+DROP TABLE lecture;
+
+DROP TABLE lecturer;
+
+DROP TABLE expense;
+
+DROP TABLE journal;
+
+DROP TABLE academics;
+
+DROP TABLE papers;
+
+DROP TABLE project;
+
+DROP TABLE researcher;
