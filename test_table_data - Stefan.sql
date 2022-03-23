de9c3753ec5376ef0bf493173a22d7f59ed1395f
Stefan Ahmed <32064735+stu115@users.noreply.github.com>
2022-03-23 20:49:02 +0000

    Add files via upload

---
+++ test_table_data - Stefan.sql
@@ -0,0 +1,18 @@
+/*Test data
+
+/*Lecture table
+
+INSERT INTO lecture VALUES 
+(‘WE214’, 7665, 1224, ‘20-03-17’, ‘Computer Science’)
+
+/*Attendance table
+
+INSERT INTO attendance
+VALUES 
+(2458, 1228, 6)   
+
+/*Student table
+
+INSET INTO student
+VALUES
+(2369, ‘Bethany ’, ‘Hughes’, ‘Cyber Security’) 
